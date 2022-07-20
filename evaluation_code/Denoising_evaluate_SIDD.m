99bb636143eeab63b496a4622c37362ce8397ab9
范植貿(Chi-Mao Fan) <qaz5517359@gmail.com>
2022-07-20 21:25:53 +0800

    Add files via upload

---
+++ evaluation_code/Denoising_evaluate_SIDD.m
@@ -0,0 +1,27 @@
+close all;clear all;clc
+
+denoised = load('./test_results/SIDD/mat/Idenoised.mat');
+gt = load('D:\dataset\Denoise\SIDD\test\ValidationGtBlocksSrgb.mat');
+
+denoised = denoised.Idenoised;
+gt = gt.ValidationGtBlocksSrgb;
+gt = im2single(gt);
+
+total_psnr = 0;
+total_ssim = 0;
+for i = 1:40
+    for k = 1:32
+       denoised_patch = squeeze(denoised(i,k,:,:,:));
+       gt_patch = squeeze(gt(i,k,:,:,:));
+       ssim_val = ssim(denoised_patch, gt_patch);
+       psnr_val = psnr(denoised_patch, gt_patch);
+       total_ssim = total_ssim + ssim_val;
+       total_psnr = total_psnr + psnr_val;
+    end
+   fprintf('Image: %d/40\n', i);
+end
+qm_psnr = total_psnr / (40*32);
+qm_ssim = total_ssim / (40*32);
+
+fprintf('PSNR: %f SSIM: %f\n', qm_psnr, qm_ssim);
+
